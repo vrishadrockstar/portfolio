218fdf951a7177067c0b4699f8389702dead20c5
vrishadrockstar <vrishadrockstar@gmail.com>
2021-12-14 16:15:29 +0530

    Responsive design

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -10,7 +10,10 @@ export default function Contact() {
               <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                 EMAIL
               </h2>
-              <a className="text-indigo-400 leading-relaxed">
+              <a
+                href="mailto: vrushali.joshi99@gmail.com"
+                className="text-indigo-400 leading-relaxed"
+              >
                 vrushali.joshi99@gmail.com
               </a>
             </div>
